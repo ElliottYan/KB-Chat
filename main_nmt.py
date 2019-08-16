89be40792f93d464fd0a07ce271d943f04ba4ca4
elliott <459800873@qq.com>
2019-08-16 19:10:48 +0800

    little modification

--- main_nmt.py
+++ main_nmt.py
@@ -17,25 +17,26 @@ for epoch in range(300):
     # Run the train function
     pbar = tqdm(enumerate(train),total=len(train))
     for i, data in pbar: 
-        model.train_batch(input_batches=data[0], 
-                          input_lengths=data[1], 
-                          target_batches=data[2], 
-                          target_lengths=data[3], 
-                          target_index=data[4], 
-                          batch_size=len(data[1]),
-                          clip= 10.0,
-                          teacher_forcing_ratio=0.5,
-                          i==0)
+        # model.train_batch(input_batches=data[0],
+        #                   input_lengths=data[1],
+        #                   target_batches=data[2],
+        #                   target_lengths=data[3],
+        #                   target_index=data[4],
+        #                   batch_size=len(data[1]),
+        #                   clip= 10.0,
+        #                   teacher_forcing_ratio=0.5,
+        #                   i==0)
+        #
+        # pbar.set_description(model.print_loss())
+        continue
 
-        pbar.set_description(model.print_loss())
-
-    if((epoch+1) % 1 == 0):    
-        bleu = model.evaluate(train,avg_best)
-        model.scheduler.step(bleu)
-        if(bleu >= avg_best):
-            avg_best = bleu
-            cnt=0
-        else:
-            cnt+=1
-
-        if(cnt == 5): break
+    # if((epoch+1) % 1 == 0):
+    #     bleu = model.evaluate(train,avg_best)
+    #     model.scheduler.step(bleu)
+    #     if(bleu >= avg_best):
+    #         avg_best = bleu
+    #         cnt=0
+    #     else:
+    #         cnt+=1
+    #
+    #     if(cnt == 5): break
